5a31a87674453a701a4e322bda0b765246fcc388
mattperkins <github@mattperkins.com>
2017-11-13 21:33:51 +0000

    implement washpo data fetch and html template table format

---
+++ web_templates-r1.go
@@ -0,0 +1,68 @@
+package main
+
+import ("fmt"
+		"net/http"
+		"html/template"
+		"io/ioutil"
+		"encoding/xml")
+
+type SitemapIndex struct {
+	Locations []string `xml:"sitemap>loc"`
+}
+
+type News struct {
+	Titles []string `xml:"url>news>title"`
+	Keywords []string `xml:"url>news>keywords"`
+	Locations []string `xml:"url>loc"`
+}
+
+type NewsMap struct {
+	Keyword string
+	Location string
+}	
+
+type NewsAggPage struct{
+	Title string
+	News map[string]NewsMap
+}
+
+func newsAggHandler(w http.ResponseWriter, r *http.Request){
+	var s SitemapIndex
+	var n News
+	news_map := make(map[string]NewsMap) //key=string value=NewsMap
+	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemap-index.xml")
+	bytes, _ := ioutil.ReadAll(resp.Body)
+	xml.Unmarshal(bytes, &s)
+
+	for _, Location := range s.Locations{
+		resp, _ := http.Get(Location)
+		bytes, _ := ioutil.ReadAll(resp.Body)
+		xml.Unmarshal(bytes, &n)
+		for idx, _ := range n.Titles{
+			news_map[n.Titles[idx]] = NewsMap{n.Keywords[idx], n.Locations[idx]}
+		} 
+	}
+	for idx, data := range news_map{
+		 fmt.Println("\n\n\n",idx)
+		 fmt.Println("\n",data.Keyword)
+		 fmt.Println("\n",data.Location)
+	 }
+
+	p := NewsAggPage{Title: "WashPo Aggregator", News: news_map}
+	t, _ := template.ParseFiles("template-r1.html")
+	// fmt.Println(t.Execute(w, p)) // check for errors in template.html
+	t.Execute(w, p)
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request){
+	fmt.Fprintf(w, "<a href='/agg'><h1>Get The News</h1></a>")
+}
+
+func main() {
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/agg", newsAggHandler)
+	fmt.Println("Running at: http://localhost:8000")
+	http.ListenAndServe(":8000", nil)
+}
+
+ 
