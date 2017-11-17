4f9869e12349c510548345bc9f0feee48917c646
mattperkins <github@mattperkins.com>
2017-11-17 11:17:51 +0000

    test

--- test/test.go
+++ test/test.go
@@ -2,14 +2,20 @@ package main
 
 import (
 	"fmt"
-	"strings"
+	"math"
+	"math/big"
 )
 
 func main() {
-	a := 1
-	for a < 5 {
-		fmt.Println("Number:", a)
-		a++
-	}
-	fmt.Println(strings.ToUpper("Hello"))
+	var b1, b2, b3, bigSum big.Float
+	b1.SetFloat64(65.233)
+	b2.SetFloat64(987.233)
+	b3.SetFloat64(22.33)
+
+	bigSum.Add(&b1, &b2).Add(&bigSum, &b3)
+	fmt.Printf("BigSum = %.10g\n", &bigSum)
+
+	circleRadius := 15.5
+	circumference := circleRadius * math.Pi
+	fmt.Printf("Circumference: %.1f\n", circumference)
 }
